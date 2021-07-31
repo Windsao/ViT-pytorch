4fe121b632514e82dd7b7c2d4a3f30ad625960c6
Windsao <swu23@gwu.edu>
2021-07-31 01:07:12 -0500

    update script

--- test.sh
+++ test.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICE=9 python3 train.py --name cifar10-100_500 --dataset cifar10 --model_type ViT-B_16 --pretrained_dir checkpoint/ViT-B_16.npz
+CUDA_VISIBLE_DEVICE=9 python3 train.py --name cifar10-100_500 --dataset cifar10 --gradient_accumulation_steps 20 --model_type ViT-B_16 --pretrained_dir checkpoint/ViT-B_16.npz > testlog.txt 2 > &1 &
